8d97cf9707d607b6d4ed41b57ea0bcb54c8357da
Bhargavwelcome <160607599+Bhargavwelcome@users.noreply.github.com>
2025-11-11 16:22:59 +0530

    Update webpush-service-worker.js

--- webpush-service-worker.js
+++ webpush-service-worker.js
@@ -1,3 +1,15 @@
+// Import the Responsys Web Push SDK for Service Workers
 importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
-var config = '{"appserviceKey":"BBfCgd8DzPawmPJgGJy8decXP1TJvDczsIpA0wqk961Xjb3auyJ9qMC2JwPT6XiRK429JsCDtWRgyLoaujPeGmE=","apiKey":"ABEWBYcAAX3LV04gEHMQJ2xIs","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
-orawp.init(config);
+
+// Configuration for your Web Push App
+var config = {
+    appserviceKey: "BBfCgd8DzPawmPJgGJy8decXP1TJvDczsIpA0wqk961Xjb3auyJ9qMC2JwPT6XiRK429JsCDtWRgyLoaujPeGmE=",
+    apiKey: "ABEWBYcAAX3LV04gEHMQJ2xIs",
+    accountToken: "ABElKaqpK-3l_zDeOp-XArlkg",
+    appver: "0.0.0",
+    apiHost: "https://abr16c0-webpush.oraclersys.com",
+    lazy: false
+};
+
+// Initialize the Responsys Web Push SDK
+orawp.init(JSON.stringify(config));
