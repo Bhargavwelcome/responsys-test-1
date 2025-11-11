a1a687e272ddfa25ee56850aa340f609592f1455
Bhargavwelcome <160607599+Bhargavwelcome@users.noreply.github.com>
2025-11-11 15:03:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
+// service-worker.js
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+
+const config = {
+  appserviceKey: "BBfCgd8DzPawmPJgGJy8decXP1TJvDczsIpA0wqk961Xjb3auyJ9qMC2JwPT6XiRK429JsCDtWRgyLoaujPeGmE=",
+  apiKey: "ABEWBYcAAX3LV04gEHMQJ2xIs",
+  accountToken: "ABElKaqpK-3l_zDeOp-XArlkg",
+  apiHost: "https://abr16c0-webpush.oraclersys.com",
+  appver: "1.0.0",
+  lazy: false
+};
+
+// Initialize SDK in the Service Worker
+orawp.init(config);
 
