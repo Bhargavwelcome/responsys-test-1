340f673e1026bc4be69956f53050d31ce4c7bbb7
Bhargavwelcome <160607599+Bhargavwelcome@users.noreply.github.com>
2025-11-11 13:31:33 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,1 @@
-// Load Responsys SDK for handling push events only
-importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
 
-//  DO NOT CALL orawp.init(config) HERE!
-// The main page already initializes the SDK.
-// This worker will automatically receive messages via the imported SDK.
