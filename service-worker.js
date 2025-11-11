9e5ba383888ff3c15ed4c963b5358ce64591ad44
Bhargavwelcome <160607599+Bhargavwelcome@users.noreply.github.com>
2025-11-11 17:27:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
+// Import the Responsys Web Push SDK
 importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
-var config = '{"appserviceKey":"BBfCgd8DzPawmPJgGJy8decXP1TJvDczsIpA0wqk961Xjb3auyJ9qMC2JwPT6XiRK429JsCDtWRgyLoaujPeGmE=","apiKey":"ABEWBYcAAX3LV04gEHMQJ2xIs","accountToken":"ABElKaqpK-3l_zDeOp-XArlkg","appver":"0.0.0","apiHost":"https://abr16c0-webpush.oraclersys.com","lazy":false}';
+
+// SDK configuration
+var config = {
+  appserviceKey: "BBfCgd8DzPawmPJgGJy8decXP1TJvDczsIpA0wqk961Xjb3auyJ9qMC2JwPT6XiRK429JsCDtWRgyLoaujPeGmE=",
+  apiKey: "ABEWBYcAAX3LV04gEHMQJ2xIs",
+  accountToken: "ABElKaqpK-3l_zDeOp-XArlkg",
+  apiHost: "https://abr16c0-webpush.oraclersys.com",
+  lazy: false
+};
+
+// Initialize SDK
 orawp.init(JSON.stringify(config));
